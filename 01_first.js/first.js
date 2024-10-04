a42da1042b49a0d2a2c41f32ab70102632a552c7
vijayji747 <kumarvijayvk747@gmail.com>
2024-10-04 10:25:55 +0000

    Variable data done

--- 01_first.js/first.js
+++ 01_first.js/first.js
@@ -2,7 +2,12 @@ const accountID =123456
 let accountImail ="abc@gmail.com"
 var accountPassword ="4321"
 accountCity = "bansi"
+let accountState ="uttar pradesh";
 
 // accountID =3 // Not allowed
 
-console.log(accountID);
+accountImail = "vjay@gmail.com"
+accountPassword = "0987"
+accountCity = "siddharth nagar"
+
+console.table([accountID, accountImail, accountPassword, accountCity, accountState ]);
