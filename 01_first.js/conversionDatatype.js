11e393e141294b22224ccb44072d867b50544815
vijayji747 <kumarvijayvk747@gmail.com>
2024-10-06 01:45:16 +0000

    conversion of datatype

---
+++ 01_first.js/conversionDatatype.js
@@ -0,0 +1,30 @@
+let score =""
+
+// console.log(typeof score);
+// console.log(typeof (score));
+
+let valueInNumber = Number(score)
+// console.log(valueInNumber);
+// console.log(typeof valueInNumber);
+
+// "33" => 33
+// "33abc" => NaN
+// ture => 1
+// flase = 0
+
+let IsLoggedIn = "vijay"
+
+let booleanIsLoggedIn = Boolean(IsLoggedIn)
+
+// console.log(booleanIsLoggedIn);
+
+// 1 => true
+// "" => false
+// "vijay" => ture
+
+let someNumber =22
+
+let StringsomeNumber = String(someNumber)
+console.log(StringsomeNumber);
+console.log(typeof StringsomeNumber);
+
