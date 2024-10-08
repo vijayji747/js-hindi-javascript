5fc4827e4f548cc6915a7f99a6f4eb0f3a73b1d8
vijayji747 <kumarvijayvk747@gmail.com>
2024-10-08 01:06:54 +0000

    datatype summery

---
+++ 01_first.js/datatypeSummery.js
@@ -0,0 +1,36 @@
+// primitive datatype
+
+// 7 type => string, number, boolean, null, undefined, symbol, bigInt
+
+const score = 100
+const scoreValue = 100.3
+const isLogedId =  false
+const outsideTemp = null
+let userEmail;
+
+const id = Symbol('123')
+
+const anotherId = Symbol('123')
+
+console.log(id === anotherId);
+
+const bigNumber = 1234567890987654321n
+
+console.log(typeof bigNumber);
+
+
+// Reference (Non primitive datatype)
+
+// Array, Objects, function
+
+Array ["shaktiman", "nagraj", "doga"];
+
+let myObj {
+    name: = "vijay",
+    age: 23,
+}
+
+
+const myFuntion = function(){
+    console.log("hello world");
+    }
